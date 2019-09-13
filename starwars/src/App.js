2843e0e5002f25cdc89f5167a1f16876e938f243
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-09-13 08:07:13 +0000

    Initial commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+import People from "./components/People";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  // https://swapi.co/api/people
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people")
+      .then(res => {
+        let characters = res.data.results;
+        setPeople(characters);
+        console.log(characters);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(people);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <People />
     </div>
   );
-}
+};
 
 export default App;
---
+++ starwars/src/components/People.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Person from "./Person";
+
+const People = () => {
+  return (
+    <div>
+      <h1>People component</h1>
+      <Person />
+    </div>
+  );
+};
+
+export default People;
